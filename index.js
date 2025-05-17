1d6421e73d00ad2674d3e940d46cbba19e48a106
Diego Serra <diegoserra120@hotmail.com>
2025-05-17 16:26:34 -0300

    inicio projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const ua = req.headers['user-agent'];
+
+  if (/android/i.test(ua)) {
+    return res.redirect('https://play.google.com/store/apps/details?id=com.seuapp.android');
+  } else if (/iphone|ipad|ipod/i.test(ua)) {
+    return res.redirect('https://play.google.com/store/apps/details?id=xpss.pralapraka');
+  } else {
+    return res.send("Sistema não reconhecido. Apenas Android e iOS são suportados.");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
